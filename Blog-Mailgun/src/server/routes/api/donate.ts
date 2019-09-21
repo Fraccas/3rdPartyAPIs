6c92677ca1084eac6a46b9b020cf3f3f671228f6
Fraccas <daltonward1@yahoo.com>
2019-09-21 10:35:39 -0500

    Started Mailgun Lab

---
+++ Blog-Mailgun/src/server/routes/api/donate.ts
@@ -0,0 +1,28 @@
+import * as express from 'express';
+import * as stripeLoader from 'stripe';
+
+const router = express.Router();
+
+const stripe = new stripeLoader('*********');
+
+const charge = (token: string, amt: number) => {
+    return stripe.charges.create({
+        amount: amt * 100,
+        currency: 'usd',
+        source: token,
+        description: 'Statement from stripe...'
+    });
+}
+
+router.post('/', async (req, res, next) => {
+    try {
+        let data = await charge(req.body.token.id, req.body.amount);
+        console.log(data);
+        res.send("Charged!");
+    } catch (e) {
+        console.log(e);
+        res.status(500);
+    }
+})
+
+export default router;
